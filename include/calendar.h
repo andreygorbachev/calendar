4082b8b4d1b5ce2a73e979d865cbffd7996b61b7
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-03-24 22:22:29 +0000

    Add constexpr.

--- include/calendar.h
+++ include/calendar.h
@@ -15,15 +15,19 @@ public:
 
 public:
 
-	basic_calendar() noexcept;
+	constexpr basic_calendar() noexcept;
 
-	explicit basic_calendar(weekend_storage weekend) noexcept;
+	constexpr explicit basic_calendar(weekend_storage weekend) noexcept;
 
 public:
 
-	auto is_weekend(const std::chrono::year_month_day& ymd) const noexcept -> bool;
+	constexpr bool operator==(const basic_calendar&) const noexcept = default;
 
-	auto is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool;
+public:
+
+	constexpr auto is_weekend(const std::chrono::year_month_day& ymd) const noexcept -> bool;
+
+	constexpr auto is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool;
 
 private:
 
@@ -33,25 +37,25 @@ private:
 
 
 
-inline basic_calendar::basic_calendar() noexcept : _weekend{}
+constexpr basic_calendar::basic_calendar() noexcept : _weekend{}
 {
 	_weekend[std::chrono::Saturday.c_encoding()] = true;
 	_weekend[std::chrono::Sunday.c_encoding()] = true;
 }
 
-inline basic_calendar::basic_calendar(weekend_storage weekend) noexcept : _weekend{ std::move(weekend) }
+constexpr basic_calendar::basic_calendar(weekend_storage weekend) noexcept : _weekend{ std::move(weekend) }
 {
 }
 
 
-inline auto basic_calendar::is_weekend(const std::chrono::year_month_day& ymd) const noexcept -> bool
+constexpr auto basic_calendar::is_weekend(const std::chrono::year_month_day& ymd) const noexcept -> bool
 {
 	const auto wd = std::chrono::weekday{ ymd };
 
 	return _weekend[wd.c_encoding()];
 }
 
-inline auto basic_calendar::is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool
+constexpr auto basic_calendar::is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool
 {
 	return !is_weekend(ymd);
 }
--- test/basic_calendar.cpp
+++ test/basic_calendar.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-	const auto c1 = basic_calendar{}; // Saturday/Sunday
-	const auto c2 = basic_calendar{ basic_calendar::weekend_storage{} }; // no weekend
+	constexpr auto c1 = basic_calendar{}; // Saturday/Sunday
+	constexpr auto c2 = basic_calendar{ basic_calendar::weekend_storage{} }; // no weekend
 
 	static_assert(c1 != c2);
 }
