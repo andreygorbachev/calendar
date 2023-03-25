5c332ff8e7f8c9aa0eb4a3e46b9b8a87273e5d0f
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-03-25 09:06:27 +0000

    Better comment.

--- test/basic_calendar.cpp
+++ test/basic_calendar.cpp
@@ -5,7 +5,7 @@ using namespace std::chrono;
 
 int main()
 {
-	constexpr auto c1 = basic_calendar{}; // Saturday/Sunday
+	constexpr auto c1 = basic_calendar{}; // weekend on Saturday/Sunday
 	constexpr auto c2 = basic_calendar{ basic_calendar::weekend_storage{} }; // no weekend
 
 	static_assert(c1 != c2);
