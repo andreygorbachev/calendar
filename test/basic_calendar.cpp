de149b6cd9148fc0bbf0c97edbb29ffb93a95fc6
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-03-25 08:55:34 +0000

    More tests.

--- test/basic_calendar.cpp
+++ test/basic_calendar.cpp
@@ -1,5 +1,7 @@
 #include "..\include\calendar.h"
 
+using namespace std::chrono;
+
 
 int main()
 {
@@ -7,4 +9,20 @@ int main()
 	constexpr auto c2 = basic_calendar{ basic_calendar::weekend_storage{} }; // no weekend
 
 	static_assert(c1 != c2);
+
+	// Friday
+	static_assert(c1.is_business_day({ 2023y/March/24 }));
+	static_assert(c2.is_business_day({ 2023y/March/24 }));
+
+	// Saturday
+	static_assert(!c1.is_business_day({ 2023y/March/25 }));
+	static_assert(c2.is_business_day({ 2023y/March/25 }));
+
+	// Sunday
+	static_assert(!c1.is_business_day({ 2023y/March/26 }));
+	static_assert(c2.is_business_day({ 2023y/March/26 }));
+
+	// Monday
+	static_assert(c1.is_business_day({ 2023y/March/27 }));
+	static_assert(c2.is_business_day({ 2023y/March/27 }));
 }
