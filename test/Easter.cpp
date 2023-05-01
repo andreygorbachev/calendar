71e3604caeef203e6aea723d7cc555870245cdf2
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-01 13:24:10 +0100

    One more test for Easter.

--- test/Easter.cpp
+++ test/Easter.cpp
@@ -5,10 +5,15 @@ using namespace std::chrono;
 
 constexpr auto Easter_tests() -> bool
 {
-	constexpr auto e_calculated = get_Easter(2023y);
-	constexpr auto e_expected = 2023y/April/9d;
+	constexpr auto e_calculated1 = get_Easter(1961y);
+	constexpr auto e_expected1 = 1961y/April/2d;
 
-	static_assert(e_calculated == e_expected);
+	static_assert(e_calculated1 == e_expected1);
+
+	constexpr auto e_calculated2 = get_Easter(2023y);
+	constexpr auto e_expected2 = 2023y/April/9d;
+
+	static_assert(e_calculated2 == e_expected2);
 }
 
 
