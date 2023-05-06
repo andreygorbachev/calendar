e1abfcbe9d54a2c3e97069462afb4bdf62b70dcc
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-06 17:47:33 +0100

    Started to think about business days.

---
+++ include/business_days.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "calendar.h"
+
+
+
+class business_days
+{
+
+public:
+
+	explicit business_days(const calendar& c);
+
+};
+
+
+
+business_days::business_days(const calendar& c)
+{
+
+}
---
+++ test/business_days.cpp
@@ -0,0 +1,15 @@
+#include <business_days.h>
+
+using namespace std::chrono;
+
+
+
+constexpr void business_days_calendar_tests()
+{
+}
+
+
+int main()
+{
+	business_days_calendar_tests();
+}
