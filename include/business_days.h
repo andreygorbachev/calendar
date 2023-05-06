077ab38d6bb1ffd55bf2a6ad38934b0f463cba18
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-06 18:01:49 +0100

    More is_business_day.

--- include/business_days.h
+++ include/business_days.h
@@ -2,6 +2,8 @@
 
 #include "calendar.h"
 
+#include <chrono>
+
 
 
 class business_days
@@ -9,13 +11,27 @@ class business_days
 
 public:
 
-	explicit business_days(const calendar& c);
+	constexpr explicit business_days(const calendar* cal);
+
+public:
+
+	constexpr auto is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool;
+
+private:
+
+	const calendar* _calendar;
 
 };
 
 
 
-business_days::business_days(const calendar& c)
+constexpr business_days::business_days(const calendar* cal) : _calendar{ cal }
 {
+}
 
+
+constexpr auto business_days::is_business_day(const std::chrono::year_month_day& ymd) const noexcept -> bool
+{
+	return _calendar->is_business_day(ymd);
+	// does it mean that is_business_day should be removed from calendar? (and only exist here)
 }
--- test/business_days.cpp
+++ test/business_days.cpp
@@ -3,13 +3,32 @@
 using namespace std::chrono;
 
 
-
-constexpr void business_days_calendar_tests()
+constexpr auto make_May_London_calendar() -> calendar
 {
+	auto hols = calendar::holidays_storage{
+		2023y / May / 1d,
+		2023y / May / 8d,
+		2023y / May / 29d,
+	};
+
+	return calendar{ 2023y / May / 1d, 2023y / May / 31d, move(hols) };
+}
+
+
+constexpr auto business_days_calendar_tests() -> bool
+{
+	const auto c1 = make_May_London_calendar();
+
+	const auto test1 =
+		!c1.is_business_day(2023y/May/1d) &&
+		c1.is_business_day(2023y/May/2d);
+
+	return
+		test1;
 }
 
 
 int main()
 {
-	business_days_calendar_tests();
+	static_assert(business_days_calendar_tests());
 }
