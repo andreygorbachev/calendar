42c25f0e887ec219a9c61cd2111dfb4bb0446e4c
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-07 16:00:34 +0100

    Move more tests to gtest.

--- test/Easter.cpp
+++ test/Easter.cpp
@@ -1,28 +1,19 @@
 #include <holiday.h>
 
+#include <gtest/gtest.h>
+
 #include <chrono>
 
 using namespace std::chrono;
 
 
-
-consteval auto Easter_tests() -> bool
+namespace
 {
-//	constexpr auto e_calculated1 = get_Easter(1961y);
-	constexpr auto e_expected1 = 1961y/April/2d;
 
-//	static_assert(e_calculated1 == e_expected1);
+	TEST(holiday, make_Easter)
+	{
+		EXPECT_EQ(1961y / April / 2d, make_Easter(1961y));
+		EXPECT_EQ(2023y / April / 9d, make_Easter(2023y));
+	}
 
-//	constexpr auto e_calculated2 = get_Easter(2023y);
-	constexpr auto e_expected2 = 2023y/April/9d;
-
-//	static_assert(e_calculated2 == e_expected2);
-
-	return true;
-}
-
-
-int main()
-{
-	Easter_tests();
 }
