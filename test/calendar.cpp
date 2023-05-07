1d42113a523e6dd40813469acad70c6e2ad22f41
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-07 15:51:04 +0100

    Move more tests to gtest.

--- test/calendar.cpp
+++ test/calendar.cpp
@@ -1,5 +1,7 @@
 #include <calendar.h>
 
+#include <gtest/gtest.h>
+
 #include <memory>
 #include <chrono>
 
@@ -7,83 +9,72 @@ using namespace std;
 using namespace std::chrono;
 
 
-
-consteval auto make_April_London_calendar() -> calendar
+namespace
 {
-	auto hols = calendar::holidays_storage{
-		2023y/April/7d,
-		2023y/April/10d,
-	};
 
-	return calendar{ 2023y/April/1d, 2023y/April/30d, move(hols) };
-}
+	auto make_April_London_calendar() -> calendar
+	{
+		auto hols = calendar::holidays_storage{
+			2023y / April / 7d,
+			2023y / April / 10d,
+		};
 
-consteval auto make_May_London_calendar() -> calendar
-{
-	auto hols = calendar::holidays_storage{
-		2023y/May/1d,
-		2023y/May/8d,
-		2023y/May/29d,
-	};
+		return calendar{ 2023y / April / 1d, 2023y / April / 30d, move(hols) };
+	}
 
-	return calendar{ 2023y/May/1d, 2023y/May/31d, move(hols) };
-}
+	auto make_May_London_calendar() -> calendar
+	{
+		auto hols = calendar::holidays_storage{
+			2023y / May / 1d,
+			2023y / May / 8d,
+			2023y / May / 29d,
+		};
 
-consteval auto make_May_NewYork_calendar() -> calendar
-{
-	auto hols = calendar::holidays_storage{
-		2023y/May/29d,
-	};
+		return calendar{ 2023y / May / 1d, 2023y / May / 31d, move(hols) };
+	}
 
-	return calendar{ 2023y/May/1d, 2023y/May/31d, move(hols) };
-}
+	auto make_May_NewYork_calendar() -> calendar
+	{
+		auto hols = calendar::holidays_storage{
+			2023y / May / 29d,
+		};
 
-consteval auto make_empty_calendar() -> calendar
-{
-	auto hols = calendar::holidays_storage{
-	};
+		return calendar{ 2023y / May / 1d, 2023y / May / 31d, move(hols) };
+	}
 
-	return calendar{ 2023y/May/1d, 2023y/May/31d, move(hols) };
-}
+	auto make_empty_calendar() -> calendar
+	{
+		auto hols = calendar::holidays_storage{
+		};
 
-
-
-consteval auto calendar_tests() -> bool
-{
-	const auto c1 = make_May_London_calendar();
-	const auto c2 = make_empty_calendar();
-
-	const auto test1 =
-		c1 != c2 &&
-		c1.is_holiday(2023y/May/1d) &&
-		!c1.is_weekend(2023y/May/1d) &&
-		!c1.is_business_day(2023y/May/1d) &&
-		c1.is_business_day(2023y/May/2d);
-
-	const auto c3 = make_May_NewYork_calendar() | c1;
-	const auto c4 = make_May_NewYork_calendar() & c1;
-
-	const auto test2 =
-		c3 != c1 &&
-		c3.is_holiday(2023y/May/1d) &&
-		c4 != c1 &&
-		c4 != c3 &&
-		!c4.is_holiday(2023y/May/1d);
-	// add tests for weekend as well
-
-	const auto c5 = make_April_London_calendar() + c1;
-
-	const auto test3 =
-		c3.is_holiday(2023y/May/1d);
-
-	return 
-		test1 &&
-		test2 &&
-		test3;
-}
+		return calendar{ 2023y / May / 1d, 2023y / May / 31d, move(hols) };
+	}
 
 
-int main()
-{
-	static_assert(calendar_tests());
+	TEST(calendar, operator_not_equal)
+	{
+		const auto c1 = make_May_London_calendar();
+		const auto c2 = make_empty_calendar();
+		const auto c3 = make_May_NewYork_calendar() | c1;
+		const auto c4 = make_May_NewYork_calendar() & c1;
+
+		EXPECT_EQ(true, c1 != c2);
+		EXPECT_EQ(true, c1 != c3);
+		EXPECT_EQ(true, c3 != c4);
+		EXPECT_EQ(true, c1 != c4);
+	}
+
+	TEST(calendar, is_holiday)
+	{
+		const auto c1 = make_May_London_calendar();
+		const auto c3 = make_May_NewYork_calendar() | c1;
+		const auto c4 = make_May_NewYork_calendar() & c1;
+		const auto c5 = make_April_London_calendar() + c1;
+
+		EXPECT_EQ(true, c1.is_holiday(2023y / May / 1d));
+		EXPECT_EQ(true, c3.is_holiday(2023y / May / 1d));
+		EXPECT_EQ(false, c4.is_holiday(2023y / May / 1d));
+		EXPECT_EQ(true, c3.is_holiday(2023y / May / 1d));
+	}
+
 }
