c7b27e56ddd5af99bb36137463d8f546a0badcea
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-08 14:52:17 +0100

    Started on business day conventions (as they would be needed for substitute days).

---
+++ include/business_day_convention.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "calendar.h"
+
+#include <chrono>
+
+
+class business_day_convention {
+
+public:
+
+	virtual ~business_day_convention() noexcept = default;
+
+public:
+
+	virtual auto adjust(const std::chrono::year_month_day& ymd, const calendar& c) const noexcept -> std::chrono::year_month_day = 0;
+
+};
---
+++ test/business_day_convention.cpp
@@ -0,0 +1,18 @@
+#include <business_day_convention.h>
+
+#include <gtest/gtest.h>
+
+#include <chrono>
+
+using namespace std::chrono;
+
+
+namespace
+{
+
+	TEST(business_day_convention, following)
+	{
+//		EXPECT_EQ(2023y / January / 1d, NewYearsDay.holiday(2023y));
+	}
+
+}
