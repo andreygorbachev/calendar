6b5ed352472ff39503d065482846a8e3556555ed
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-05-08 14:17:51 +0100

    More consistent names.

--- test/Easter.cpp
+++ test/Easter.cpp
@@ -10,7 +10,7 @@ using namespace std::chrono;
 namespace
 {
 
-	TEST(holiday, make_Easter)
+	TEST(holiday, make_easter)
 	{
 		EXPECT_EQ(1961y / April / 2d, make_easter(1961y));
 		EXPECT_EQ(2023y / April / 9d, make_easter(2023y));
