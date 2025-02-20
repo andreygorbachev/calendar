b04ac792fdfacfdea043ca999bd4f6e99f8bef7a
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-02-20 21:57:15 +0000

    Remove duplicated code.

--- include/calendar.h
+++ include/calendar.h
@@ -234,17 +234,7 @@ namespace gregorian
 	// should we pass this one by const reference? (as we are not moving from it)
 	inline auto calendar::make_business_days_schedule(period<std::chrono::sys_days> from_until) const -> schedule
 	{
-		// naive implementation to start with - can we use iota and ranges?
-		auto s = schedule::dates();
-		for (
-			auto d = from_until.get_from();
-			d <= from_until.get_until();
-			d += std::chrono::days{ 1 }
-		)
-			if (is_business_day(d))
-				s.emplace_hint(s.cend(), d);
-
-		return schedule{ days_period{ from_until.get_from(), from_until.get_until() }, std::move(s)};
+		return make_business_days_schedule( days_period{ from_until.get_from(), from_until.get_until() });
 	}
 
 
