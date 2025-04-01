8452d604656bd5308d0f91d288ad710c4224c36c
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-04-01 20:55:20 +0100

    ToDo comment.

--- example/ANBIMA/example.cpp
+++ example/ANBIMA/example.cpp
@@ -102,6 +102,10 @@ inline auto generate_ANBIMA_schedule() -> schedule
 
 	return s1 + s2;
 	// please note that holidays are not adjusted in this example
+
+	// I think some of the days should be adjusted (but not all of them)
+	// so we should probably generate 2 parts of the claendar separately and then merge them
+	// (after adjusting one of them as required)
 }
 
 
