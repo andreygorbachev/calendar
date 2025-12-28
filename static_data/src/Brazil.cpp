b51af11ec269de6f047fd4d8376721741219a89d
Andrey Gorbachev <andreygorbachev@hotmail.com>
2025-12-28 18:34:57 +0000

    ToDo comment.

--- static_data/src/Brazil.cpp
+++ static_data/src/Brazil.cpp
@@ -58,7 +58,7 @@ namespace gregorian
 		{
 			const annual_holiday* holiday;
 			days_period period{ Epoch };
-			days_period announced_cancelled{ Epoch }; // do we need time as well? // is creation and cancellation immidiately effective?
+			days_period announced_cancelled{ Epoch }; // do we need time as well? // is creation and cancellation immidiately effective (how tzdata deals with the same)?
 		};
 		// how do we handle a situation when Saturday "moves" to another day (and actually is a business day)?
 
