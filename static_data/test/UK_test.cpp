bf995a6ac7de1381c943845dd6d1857047ebf87c
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-04-07 12:46:33 +0100

    More unit tests.

---
+++ static_data/test/UK_test.cpp
@@ -0,0 +1,113 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Andrey Gorbachev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <static_data.h>
+
+#include <gtest/gtest.h>
+
+#include <schedule.h>
+
+#include <chrono>
+#include <stdexcept>
+
+using namespace std;
+using namespace std::chrono;
+
+
+namespace gregorian
+{
+
+	namespace static_data
+	{
+
+		// I guess as we treat all data as code, we should unit test it as well
+		TEST(static, England)
+		{
+			// for starters we only know about 1 "special" non-business day
+//			const auto& cal0 = locate_calendar("Europe/London", 2010y / January / 10d);
+			const auto& cal0 = locate_calendar("Europe/London", Epoch.get_from());
+			EXPECT_TRUE(cal0.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_FALSE(cal0.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_FALSE(cal0.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal0.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal0.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal1a = locate_calendar("Europe/London", 2019y / June / 6d);
+			EXPECT_TRUE(cal1a.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_FALSE(cal1a.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_FALSE(cal1a.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal1a.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal1a.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal1b = locate_calendar("Europe/London", 2019y / June / 7d); // so the new calendar is available from the day of announcement, which might or might not be what we want
+			EXPECT_TRUE(cal1b.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal1b.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_FALSE(cal1b.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal1b.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal1b.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal2a = locate_calendar("Europe/London", 2020y / November / 11d);
+			EXPECT_TRUE(cal2a.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal2a.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_FALSE(cal2a.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal2a.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal2a.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal2b = locate_calendar("Europe/London", 2020y / November / 12d);
+			EXPECT_TRUE(cal2b.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal2b.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_TRUE(cal2b.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal2b.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal2b.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal3a = locate_calendar("Europe/London", 2022y / September / 9d);
+			EXPECT_TRUE(cal3a.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal3a.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_TRUE(cal3a.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_FALSE(cal3a.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal3a.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal3b = locate_calendar("Europe/London", 2022y / September / 10d);
+			EXPECT_TRUE(cal3b.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal3b.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_TRUE(cal3b.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_TRUE(cal3b.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal3b.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal4a = locate_calendar("Europe/London", 2022y / November / 5d);
+			EXPECT_TRUE(cal4a.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal4a.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_TRUE(cal4a.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_TRUE(cal4a.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_FALSE(cal4a.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+
+			const auto& cal4b = locate_calendar("Europe/London", 2022y / November / 6d);
+			EXPECT_TRUE(cal4b.is_non_business_day(2012y / June / 5d)); // Queen's Diamond Jubilee
+			EXPECT_TRUE(cal4b.is_non_business_day(2020y / May / 8d)); // 75th anniversary of VE Day
+			EXPECT_TRUE(cal4b.is_non_business_day(2022y / June / 3d)); // Queen’s Platinum Jubilee
+			EXPECT_TRUE(cal4b.is_non_business_day(2022y / September / 19d)); // State Funeral of Queen Elizabeth II
+			EXPECT_TRUE(cal4b.is_non_business_day(2023y / May / 8d)); // coronation of King Charles III
+		}
+
+	}
+
+}
