6479c5ef9a37a2b7954b9c3de726d37c003acefb
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-03 17:29:44 +0100

    Add 2015 to CNBV.

--- static_data/src/Mexico.cpp
+++ static_data/src/Mexico.cpp
@@ -83,6 +83,22 @@ namespace gregorian
 				2006y / December / 12d,
 				2006y / December / 25d,
 
+				// from "DISPOSICIONES DE CARÁCTER GENERAL QUE SEÑALAN LOS DÍAS DEL AÑO 2015, EN QUE LAS
+				// ENTIDADES FINANCIERAS SUJETAS A LA SUPERVISIÓN DE LA COMISIÓN NACIONAL BANCARIA Y
+				// DE VALORES, DEBERÁN CERRAR SUS PUERTAS Y SUSPENDER OPERACIONES"
+
+				2015y / January / 1d, // I. El 1 de enero.
+				2015y / February / 2d, // II. El primer lunes de febrero en conmemoración del 5 de febrero, es decir, el 2 de febrero.
+				2015y / March / 16d, // III. El tercer lunes de marzo en conmemoración del 21 de marzo, es decir, el 16 de marzo.
+				2015y / April / 2d, // IV. El 2 y 3 de abril.
+				2015y / April / 3d,
+				2015y / May / 1d, // V. El 1 de mayo.
+				2015y / September / 16d, // VI. El 16 de septiembre.
+				2015y / November / 2d, // VII. El 2 de noviembre. Adicionalmente, el tercer lunes de dicho mes en conmemoración del 20 de noviembre, es decir, el 16 de noviembre.
+				2015y / November / 16d,
+				2015y / December / 12d, // VIII. El 12 y 25 de diciembre.
+				2015y / December / 25d,
+
 				// from "DISPOSICIONES DE CARÁCTER GENERAL QUE SEÑALAN LOS DÍAS DEL AÑO 2016, EN QUE LAS
 				// ENTIDADES FINANCIERAS SUJETAS A LA SUPERVISIÓN DE LA COMISIÓN NACIONAL BANCARIA Y
 				// DE VALORES, DEBERÁN CERRAR SUS PUERTAS Y SUSPENDER OPERACIONES"
@@ -265,7 +281,7 @@ namespace gregorian
 			};
 
 			return schedule{
-				days_period{ 2016y / FirstDayOfJanuary, 2026y / LastDayOfDecember },
+				days_period{ 2015y / FirstDayOfJanuary, 2026y / LastDayOfDecember },
 				std::move(holidays)
 			};
 		}
