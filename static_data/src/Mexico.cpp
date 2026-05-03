0bdf613ac3b323f99ce62e1ea6dc557eea364141
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-03 17:06:53 +0100

    Add 2019 to CNBV.

--- static_data/src/Mexico.cpp
+++ static_data/src/Mexico.cpp
@@ -83,6 +83,22 @@ namespace gregorian
 				2006y / December / 12d,
 				2006y / December / 25d,
 
+				// from "DISPOSICIONES DE CARÁCTER GENERAL QUE SEÑALAN LOS DÍAS DEL AÑO 2019, EN QUE LAS
+				// ENTIDADES FINANCIERAS SUJETAS A LA SUPERVISIÓN DE LA COMISIÓN NACIONAL BANCARIA Y
+				// DE VALORES, DEBERÁN CERRAR SUS PUERTAS Y SUSPENDER OPERACIONES"
+
+				2019y / January / 1d, // I. El 1 de enero.
+				2019y / February / 4d, // II. El primer lunes de febrero en conmemoración del 5 de febrero, es decir, el 4 de febrero.
+				2019y / March / 18d, // III. El tercer lunes de marzo en conmemoración del 21 de marzo, es decir, el 18 de marzo.
+				2019y / April / 18d, // IV. El 18 y 19 de abril.
+				2019y / April / 19d,
+				2019y / May / 1d, // V. El 1 de mayo.
+				2019y / September / 16d, // VI. El 16 de septiembre.
+				2019y / November / 2d, // VII. El 2 de noviembre.Adicionalmente, el tercer lunes de noviembre en conmemoración del 20 de noviembre, es decir, el 18 de noviembre.
+				2019y / November / 18d,
+				2019y / December / 12d, // VIII. El 12 y 25 de diciembre.
+				2019y / December / 25d,
+
 				// from "DISPOSICIONES DE CARÁCTER GENERAL QUE SEÑALAN LOS DÍAS DEL AÑO 2020, EN QUE LAS
 				// ENTIDADES FINANCIERAS SUJETAS A LA SUPERVISIÓN DE LA COMISIÓN NACIONAL BANCARIA Y
 				// DE VALORES, DEBERÁN CERRAR SUS PUERTAS Y SUSPENDER OPERACIONES"
@@ -199,7 +215,7 @@ namespace gregorian
 			};
 
 			return schedule{
-				days_period{ 2021y / FirstDayOfJanuary, 2026y / LastDayOfDecember },
+				days_period{ 2019y / FirstDayOfJanuary, 2026y / LastDayOfDecember },
 				std::move(holidays)
 			};
 		}
