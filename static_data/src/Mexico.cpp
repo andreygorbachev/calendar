f339a3cb3d16d62787b292aa0ab51c47f59ee4ff
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-03 23:07:29 +0100

    Correct the previous checkin.

--- static_data/src/Mexico.cpp
+++ static_data/src/Mexico.cpp
@@ -143,8 +143,8 @@ namespace gregorian
 				2010y / April / 1d, // IV. El 1 y 2 de abril.
 				2010y / April / 2d,
 				2010y / May / 1d, // V. El 1 de mayo.
-//				2010y / September / 16d, // VI. El 16 de septiembre.
-				2010y / September / 17d, // find a reference to why that moved (again we are not yet handling the histical calendars)
+				2010y / September / 16d, // VI. El 16 de septiembre.
+				2010y / September / 17d, // find a reference to why this additional holiday was created (again we are not yet handling the histical calendars)
 				2010y / November / 2d, // VII. El 2 de noviembre. Adicionalmente, el tercer lunes de dicho mes en conmemoración del 20 de noviembre, es decir, el 15 de noviembre.
 				2010y / November / 15d,
 				2010y / December / 12d, // VIII. El 12 y 25 de diciembre.
