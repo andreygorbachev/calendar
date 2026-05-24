5c15c0fa6b54576a0e96677c205169012446cdab
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-05-24 10:04:47 +0100

    Add  new Christmas Eve holiday in Poland.

--- static_data/src/Poland.cpp
+++ static_data/src/Poland.cpp
@@ -28,6 +28,7 @@
 #include <annual_holiday_interface.h>
 #include <annual_holidays.h>
 #include <weekend.h>
+#include <schedule.h>
 
 #include <utility>
 #include <chrono>
@@ -44,47 +45,206 @@ namespace gregorian
 	namespace static_data
 	{
 
-		auto make_Warsaw_calendar_versions() -> _calendar_versions
+		// should these be in their own namespace?
+		const auto _ThreeKingsDay = named_holiday{ January / 6d };
+		const auto _LabourDay = named_holiday{ May / 1d };
+		const auto _ConstitutionDay = named_holiday{ May / 3d };
+		const auto _CorpusChristi = offset_holiday{ &_Easter, days{ 60 } }; // should it be in the main library?
+		const auto _AssumptionDay = named_holiday{ August / 15d };
+		const auto _AllSaintsDay = named_holiday{ November / 1d };
+		const auto _IndependenceDay = named_holiday{ November / 11d };
+
+		// Pursuant to the Act of 6 December 2024 amending the Act on Public Holidays, Christmas Eve has been a statutory public holiday since 2025.
+
+		static auto _make_Warsaw_known_schedule_part0() -> schedule
 		{
-			const auto ThreeKingsDay = named_holiday{ January / 6d };
-			const auto LabourDay = named_holiday{ May / 1d };
-			const auto ConstitutionDay = named_holiday{ May / 3d };
-			const auto CorpusChristi = offset_holiday{ &_Easter, days{ 60 } }; // should it be in the main library?
-			const auto AssumptionDay = named_holiday{ August / 15d };
-			const auto AllSaintsDay = named_holiday{ November / 1d };
-			const auto IndependenceDay = named_holiday{ November / 11d };
+			auto holidays = schedule::dates{ // should we include day of the week into comments?
 
-			const auto rules = annual_holiday_storage{
-				&NewYearsDay,
-				&ThreeKingsDay,
-				&LabourDay,
-				&ConstitutionDay,
-				// &_Easter,
-				&EasterMonday,
-				&CorpusChristi,
-				&AssumptionDay,
-				&ChristmasDay,
-				&BoxingDay,
-				&AllSaintsDay,
-				&IndependenceDay
+				// Implied from PolSTR history
+
+				2022y / January / 1d, // New Year's Day
+				2022y / January / 6d, // Epiphany
+				2022y / April / 18d, // Easter Monday
+				2022y / May / 1d, // Labor Day
+				2022y / May / 3d, // Constitution Day
+				2022y / June / 16d, // Corpus Christi
+				2022y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2022y / November / 1d, // All Saints' Day
+				2022y / November / 11d, // Independence Day
+				2022y / December / 25d, // Christmas Day
+				2022y / December / 26d, // St.Stephen's Day (Second day of Christmas)
+
+				2023y / January / 1d, // New Year's Day
+				2023y / January / 6d, // Epiphany
+				2023y / April / 10d, // Easter Monday
+				2023y / May / 1d, // Labor Day
+				2023y / May / 3d, // Constitution Day
+				2023y / June / 8d, // Corpus Christi
+				2023y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2023y / November / 1d, // All Saints' Day
+				2023y / November / 11d, // Independence Day
+				2023y / December / 25d, // Christmas Day
+				2023y / December / 26d, // St.Stephen's Day
+
+				2024y / January / 1d, // New Year's Day
+				2024y / January / 6d, // Epiphany
+				2024y / April / 1d, // Easter Monday
+				2024y / May / 1d, // Labor Day
+				2024y / May / 3d, // Constitution Day
+				2024y / May / 30d, // Corpus Christi
+				2024y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2024y / November / 1d, // All Saints' Day
+				2024y / November / 11d, // Independence Day
+				2024y / December / 25d, // Christmas Day
+				2024y / December / 26d, // St.Stephen's Day
+
+				2025y / January / 1d, // New Year's Day
+				2025y / January / 6d, // Epiphany
+				2025y / April / 21d, // Easter Monday
+				2025y / May / 1d, // Labor Day
+				2025y / May / 3d, // Constitution Day
+				2025y / June / 19d, // Corpus Christi
+				2025y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2025y / November / 1d, // All Saints' Day
+				2025y / November / 11d, // Independence Day
+				2025y / December / 25d, // Christmas Day
+				2025y / December / 26d // St.Stephen's Day
 			};
 
-			const auto s = make_holiday_schedule(
-				years_period{ Epoch.get_from().year(), Epoch.get_until().year() },
-				rules
-			);
-
-			auto cal = calendar{
-				SaturdaySundayWeekend,
-				s
-			};
-
-			return {
-				{ cal.get_schedule().get_period().get_from(), std::move(cal) },
+			return schedule{
+				days_period{ 2022y / FirstDayOfJanuary, 2025y / LastDayOfDecember },
+				std::move(holidays)
 			};
 		}
 
-		// Pursuant to the Act of 6 December 2024 amending the Act on Public Holidays, Christmas Eve has been a statutory public holiday since 2025.
+		static auto _make_Warsaw_known_schedule_part1() -> schedule
+		{
+			auto holidays = schedule::dates{
+
+				// Implied from PolSTR history
+
+				2022y / January / 1d, // New Year's Day
+				2022y / January / 6d, // Epiphany
+				2022y / April / 18d, // Easter Monday
+				2022y / May / 1d, // Labor Day
+				2022y / May / 3d, // Constitution Day
+				2022y / June / 16d, // Corpus Christi
+				2022y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2022y / November / 1d, // All Saints' Day
+				2022y / November / 11d, // Independence Day
+				2022y / December / 25d, // Christmas Day
+				2022y / December / 26d, // St.Stephen's Day (Second day of Christmas)
+
+				2023y / January / 1d, // New Year's Day
+				2023y / January / 6d, // Epiphany
+				2023y / April / 10d, // Easter Monday
+				2023y / May / 1d, // Labor Day
+				2023y / May / 3d, // Constitution Day
+				2023y / June / 8d, // Corpus Christi
+				2023y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2023y / November / 1d, // All Saints' Day
+				2023y / November / 11d, // Independence Day
+				2023y / December / 25d, // Christmas Day
+				2023y / December / 26d, // St.Stephen's Day
+
+				2024y / January / 1d, // New Year's Day
+				2024y / January / 6d, // Epiphany
+				2024y / April / 1d, // Easter Monday
+				2024y / May / 1d, // Labor Day
+				2024y / May / 3d, // Constitution Day
+				2024y / May / 30d, // Corpus Christi
+				2024y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2024y / November / 1d, // All Saints' Day
+				2024y / November / 11d, // Independence Day
+				2024y / December / 25d, // Christmas Day
+				2024y / December / 26d, // St.Stephen's Day
+
+				2025y / January / 1d, // New Year's Day
+				2025y / January / 6d, // Epiphany
+				2025y / April / 21d, // Easter Monday
+				2025y / May / 1d, // Labor Day
+				2025y / May / 3d, // Constitution Day
+				2025y / June / 19d, // Corpus Christi
+				2025y / August / 15d, // Assumption of the Blessed Virgin Mary
+				2025y / November / 1d, // All Saints' Day
+				2025y / November / 11d, // Independence Day
+				2025y / December / 24d, // Christmas Eve (New public holiday)
+				2025y / December / 25d, // Christmas Day
+				2025y / December / 26d // St.Stephen's Day
+			};
+
+			return schedule{
+				days_period{ 2022y / FirstDayOfJanuary, 2025y / LastDayOfDecember },
+				std::move(holidays)
+			};
+		}
+
+
+		static auto _make_Warsaw_generated_schedule_part0() -> schedule
+		{
+			const auto rules = annual_holiday_storage{
+				&NewYearsDay,
+				&_ThreeKingsDay,
+				&_LabourDay,
+				&_ConstitutionDay,
+				&EasterMonday,
+				&_CorpusChristi,
+				&_AssumptionDay,
+				&_AllSaintsDay,
+				&_IndependenceDay,
+				&ChristmasDay,
+				&BoxingDay
+			};
+
+			return make_holiday_schedule(
+				util::years_period{ 2026y, Epoch.get_until().year() },
+				rules
+			);
+		}
+
+		static auto _make_Warsaw_generated_schedule_part1() -> schedule
+		{
+			const auto rules = annual_holiday_storage{
+				&NewYearsDay,
+				&_ThreeKingsDay,
+				&_LabourDay,
+				&_ConstitutionDay,
+				&EasterMonday,
+				&_CorpusChristi,
+				&_AssumptionDay,
+				&_AllSaintsDay,
+				&_IndependenceDay,
+				&ChristmasEve,
+				&ChristmasDay,
+				&BoxingDay
+			}; // we can make it from part0
+
+			return make_holiday_schedule(
+				util::years_period{ 2026y, Epoch.get_until().year() },
+				rules
+			);
+		}
+
+
+		auto make_Warsaw_calendar_versions() -> _calendar_versions
+		{
+			auto cal0 = calendar{
+				SaturdaySundayWeekend,
+				_make_Warsaw_known_schedule_part0() +
+				_make_Warsaw_generated_schedule_part0()
+			};
+
+			auto cal1 = calendar{
+				SaturdaySundayWeekend,
+				_make_Warsaw_known_schedule_part1() +
+				_make_Warsaw_generated_schedule_part1()
+			};
+
+			return {
+				{ cal0.get_schedule().get_period().get_from(), std::move(cal0) },
+				{ 2024y / December / 4d, std::move(cal1) },
+			};
+		}
 
 	}
 
