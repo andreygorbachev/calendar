135bbdfad56e05a0005ff22010ed32b7fe96b8eb
Andrey Gorbachev <andreygorbachev@hotmail.com>
2026-06-28 16:55:37 +0100

    See how individual years can be combined together.

--- static_data/src/USA.cpp
+++ static_data/src/USA.cpp
@@ -220,117 +220,17 @@ namespace gregorian
 
 				static auto _make_known_schedule_part0() -> schedule
 				{
-					auto holidays = schedule::dates{
-						2010y / December / 31d, // New Year's Day
-						2011y / January / 17d, // Birthday of Martin Luther King, Jr.
-						2011y / February / 21d, // Washington’s Birthday
-						2011y / May / 30d, // Memorial Day
-						2011y / July / 4d, // Independence Day
-						2011y / September / 5d, // Labor Day
-						2011y / October / 10d, // Columbus Day
-						2011y / November / 11d, // Veterans Day
-						2011y / November / 24d, // Thanksgiving Day
-						2011y / December / 26d, // Christmas Day
-
-						2012y / January / 2d, // New Year's Day
-						2012y / January / 16d, // Birthday of Martin Luther King, Jr.
-						2012y / February / 20d, // Washington’s Birthday
-						2012y / May / 28d, // Memorial Day
-						2012y / July / 4d, // Independence Day
-						2012y / September / 3d, // Labor Day
-						2012y / October / 8d, // Columbus Day
-						2012y / November / 12d, // Veterans Day
-						2012y / November / 22d, // Thanksgiving Day
-						2012y / December / 25d, // Christmas Day
-
-						2013y / January / 1d, // New Year's Day
-						2013y / January / 21d, // Birthday of Martin Luther King, Jr.
-						2013y / February / 18d, // Washington’s Birthday
-						2013y / May / 27d, // Memorial Day
-						2013y / July / 4d, // Independence Day
-						2013y / September / 2d, // Labor Day
-						2013y / October / 14d, // Columbus Day
-						2013y / November / 11d, // Veterans Day
-						2013y / November / 28d, // Thanksgiving Day
-						2013y / December / 25d, // Christmas Day
-
-						2014y / January / 1d, // New Year's Day
-						2014y / January / 20d, // Birthday of Martin Luther King, Jr.
-						2014y / February / 17d, // Washington’s Birthday
-						2014y / May / 26d, // Memorial Day
-						2014y / July / 4d, // Independence Day
-						2014y / September / 1d, // Labor Day
-						2014y / October / 13d, // Columbus Day
-						2014y / November / 11d, // Veterans Day
-						2014y / November / 27d, // Thanksgiving Day
-						2014y / December / 25d, // Christmas Day
-
-						2015y / January / 1d, // New Year's Day
-						2015y / January / 19d, // Birthday of Martin Luther King, Jr.
-						2015y / February / 16d, // Washington’s Birthday
-						2015y / May / 25d, // Memorial Day
-						2015y / July / 3d, // Independence Day
-						2015y / September / 7d, // Labor Day
-						2015y / October / 12d, // Columbus Day
-						2015y / November / 11d, // Veterans Day
-						2015y / November / 26d, // Thanksgiving Day
-						2015y / December / 25d, // Christmas Day
-
-						2016y / January / 1d, // New Year's Day
-						2016y / January / 18d, // Birthday of Martin Luther King, Jr.
-						2016y / February / 15d, // Washington’s Birthday
-						2016y / May / 30d, // Memorial Day
-						2016y / July / 4d, // Independence Day
-						2016y / September / 5d, // Labor Day
-						2016y / October / 10d, // Columbus Day
-						2016y / November / 11d, // Veterans Day
-						2016y / November / 24d, // Thanksgiving Day
-						2016y / December / 26d, // Christmas Day
-
-						2017y / January / 2d, // New Year's Day
-						2017y / January / 16d, // Birthday of Martin Luther King, Jr.
-						2017y / February / 20d, // Washington’s Birthday
-						2017y / May / 29d, // Memorial Day
-						2017y / July / 4d, // Independence Day
-						2017y / September / 4d, // Labor Day
-						2017y / October / 9d, // Columbus Day
-						2017y / November / 10d, // Veterans Day
-						2017y / November / 23d, // Thanksgiving Day
-						2017y / December / 25d, // Christmas Day
-
-						2018y / January / 1d, // New Year's Day
-						2018y / January / 15d, // Birthday of Martin Luther King, Jr.
-						2018y / February / 19d, // Washington’s Birthday
-						2018y / May / 28d, // Memorial Day
-						2018y / July / 4d, // Independence Day
-						2018y / September / 3d, // Labor Day
-						2018y / October / 8d, // Columbus Day
-						2018y / November / 12d, // Veterans Day
-						2018y / November / 22d, // Thanksgiving Day
-						2018y / December / 25d, // Christmas Day
-
-						2019y / January / 1d, // New Year's Day
-						2019y / January / 21d, // Birthday of Martin Luther King, Jr.
-						2019y / February / 18d, // Washington’s Birthday
-						2019y / May / 27d, // Memorial Day
-						2019y / July / 4d, // Independence Day
-						2019y / September / 2d, // Labor Day
-						2019y / October / 14d, // Columbus Day
-						2019y / November / 11d, // Veterans Day
-						2019y / November / 28d, // Thanksgiving Day
-						2019y / December / 25d, // Christmas Day
-
-						2020y / January / 1d, // New Year's Day
-						2020y / January / 20d, // Birthday of Martin Luther King, Jr.
-						2020y / February / 17d, // Washington’s Birthday
-						2020y / May / 25d, // Memorial Day
-						2020y / July / 3d, // Independence Day
-						2020y / September / 7d, // Labor Day
-						2020y / October / 12d, // Columbus Day
-						2020y / November / 11d, // Veterans Day
-						2020y / November / 26d, // Thanksgiving Day
-						2020y / December / 25d // Christmas Day
-					};
+					auto holidays = schedule::dates{};
+					holidays.insert(_2011.cbegin(), _2011.cend());
+					holidays.insert(_2012.cbegin(), _2012.cend());
+					holidays.insert(_2013.cbegin(), _2013.cend());
+					holidays.insert(_2014.cbegin(), _2014.cend());
+					holidays.insert(_2015.cbegin(), _2015.cend());
+					holidays.insert(_2016.cbegin(), _2016.cend());
+					holidays.insert(_2017.cbegin(), _2017.cend());
+					holidays.insert(_2018.cbegin(), _2018.cend());
+					holidays.insert(_2019.cbegin(), _2019.cend());
+					holidays.insert(_2020.cbegin(), _2020.cend());
 
 					return schedule{
 						days_period{ 2010y / December / 31d, 2020y / LastDayOfDecember }, // we pay attention to 2011y / January / 1d adjusting to 2010y / December / 31d
